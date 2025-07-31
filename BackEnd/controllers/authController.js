26896306c628977c81d371788dfe83aa0280eff9
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-01 00:11:31 +0700

    [BackEnd][Controllers] Update user register controller

---
+++ BackEnd/controllers/authController.js
@@ -0,0 +1,46 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+// Generate JWT token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+// Register User
+exports.registerUser = async (req, res) => {
+  const { fullName, email, password, profileImageUrl } = req.body;
+
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ message: "Please fill all fields" });
+  }
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+      profileImageUrl,
+    });
+
+    res.status(201).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
+  }
+};
+
+// Login User
+exports.loginUser = async (req, res) => {};
+
+// Get User Info
+exports.getUserInfo = async (req, res) => {};
