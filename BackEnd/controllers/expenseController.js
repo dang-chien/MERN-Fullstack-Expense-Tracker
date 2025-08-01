80c1e1a855a5885319109cb18066796787b052c6
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-01 12:49:58 +0700

    [Controllers] Update all logic for expense controller

---
+++ BackEnd/controllers/expenseController.js
@@ -0,0 +1,83 @@
+const XLSX = require("xlsx");
+const Expense = require("../models/Expense");
+
+// Add Expense
+exports.addExpense = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const { icon, category, amount, date } = req.body;
+
+    // Validate required fields
+    if (!category || !amount || !date) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const newExpense = new Expense({
+      userId,
+      icon,
+      category,
+      amount,
+      date: new Date(date),
+    });
+
+    await newExpense.save();
+    res.status(200).json(newExpense);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error adding expense", error: error.message });
+  }
+};
+
+// Get All Expense
+exports.getAllExpense = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const expense = await Expense.find({ userId }).sort({ date: -1 });
+    res.status(200).json(expense);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching expense", error: error.message });
+  }
+};
+
+// Delete Expense
+exports.deleteExpense = async (req, res) => {
+  try {
+    await Expense.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Expense deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting expense", error: error.message });
+  }
+};
+
+// Download Expense as Excel
+exports.downloadExpenseExcel = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const expense = await Expense.find({ userId }).sort({ date: -1 });
+
+    // Prepare data for Excel
+    const data = expense.map((item) => ({
+      Category: item.category,
+      Amount: item.amount,
+      Date: item.date.toISOString().split("T")[0],
+    }));
+
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.json_to_sheet(data);
+    XLSX.utils.book_append_sheet(wb, ws, "Expense");
+    XLSX.writeFile(wb, "expense_details.xlsx");
+    res.download("expense_details.xlsx");
+  } catch (error) {
+    res.status(500).json({
+      message: "Error downloading expense Excel",
+      error: error.message,
+    });
+  }
+};
