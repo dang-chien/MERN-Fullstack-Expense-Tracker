1426e1aa9413d25e0c329c979fa4680929e4ecd3
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-01 11:11:46 +0700

    [Feat] Add uploads folder config to store image datas

--- BackEnd/middleware/authMiddleware.js
+++ BackEnd/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User"); // Assuming you have a User model defined
+const User = require("../models/User");
 
 exports.protect = async (req, res, next) => {
   let token = req.headers.authorization?.split(" ")[1];
