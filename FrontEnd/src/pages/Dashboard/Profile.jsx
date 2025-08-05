c48e91afcbaa3ee31fae8a47ad7d5b67e8a95b8c
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-05 11:07:40 +0700

    [frontend][pages] Update Profile page with 2 feature: clear all transactions data and delete account

--- FrontEnd/src/pages/Dashboard/Profile.jsx
+++ FrontEnd/src/pages/Dashboard/Profile.jsx
@@ -1,18 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { useUserAuth } from "../../hooks/useUserAuth";
-import DashboardLayout from "../../components/layouts/DashboardLayout";
-import Input from "../../components/Inputs/Input";
+import { UserContext } from "../../context/userContext";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
+import { toast } from "react-hot-toast";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import Input from "../../components/Inputs/Input";
 import CharAvatar from "../../components/Cards/CharAvatar";
 import Textarea from "../../components/Inputs/Textarea";
 import SettingButton from "../../components/Profile/SettingButton";
+import Modal from "../../components/Modal";
+import ConfirmAlert from "../../components/ConfirmAlert";
+import { LuPencil, LuTrash2, LuBan } from "react-icons/lu";
+
 
 const Profile = () => {
     useUserAuth();
 
     const [userProfile, setUserProfile] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [clearAllTransactions, setClearAllTransactions] = useState(false);
+    const [deleteAccount, setDeleteAccount] = useState(false);
+
+    const { clearUser } = useContext(UserContext);
+    const navigate = useNavigate();
 
     const fetchUserProfile = async () => {
         if (loading) return;
@@ -23,6 +35,7 @@ const Profile = () => {
                 setUserProfile(response.data);
             }
         } catch (error) {
+            toast.error("Failed to fetch user profile. Please try again.");
             console.error("Error fetching user profile:", error);
         } finally {
             setLoading(false);
@@ -31,9 +44,34 @@ const Profile = () => {
 
     const handleUpdateProfile = async () => { };
 
-    const handleClearAllTransactions = async () => { };
+    const handleClearAllTransactions = async () => {
+        try {
+            const income_response = await axiosInstance.delete(API_PATHS.INCOME.DELETE_ALL_INCOME);
+            const expense_response = await axiosInstance.delete(API_PATHS.EXPENSE.DELETE_ALL_EXPENSE);
+            if (income_response.status === 200 && expense_response.status === 200) {
+                setClearAllTransactions(false);
+                toast.success("All transactions cleared successfully!");
+            }
+        } catch (error) {
+            console.error("Error clearing all transactions:", error);
+            toast.error("Failed to clear transactions. Please try again.");
+        }
+    };
 
-    const handleDeleteAccount = async () => { };
+    const handleDeleteAccount = async () => {
+        try {
+            const response = await axiosInstance.delete(API_PATHS.AUTH.DELETE_ACCOUNT);
+            if (response.status === 200) {
+                localStorage.removeItem("token");
+                clearUser();
+                navigate("/login");
+            }
+        } catch (error) {
+            console.error("Error deleting account:", error);
+            toast.error("Failed to delete account. Please try again.");
+            setDeleteAccount(false);
+        }
+    };
 
     useEffect(() => {
         fetchUserProfile();
@@ -45,26 +83,32 @@ const Profile = () => {
             <div className="my-5 mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className="card flex items-center justify-between flex-col">
-                        <div className="flex flex-col items-center justify-center w-full">
-                            <h2 className="text-2xl font-bold mb-2">User Profile</h2>
-                            <p className="text-gray-600 mb-4">Manage your profile settings and preferences.</p>
+                        <div className='flex items-start justify-between w-full mb-4'>
+                            <div className="flex flex-col justify-center w-full">
+                                <h2 className="text-lg font-medium mb-1">User Profile</h2>
+                                <p className="text-gray-600 mb-2">Let’s make it feel like you</p>
+                            </div>
+                            <div className="flex flex-row gap-1.5">
+                                <SettingButton tooltip="Update Your Profile" icon={<LuPencil size={18} />} onClick={handleUpdateProfile} />
+                                <SettingButton tooltip="Clear All Transactions" icon={<LuTrash2 size={18} />} onClick={() => setClearAllTransactions(true)} />
+                                <SettingButton tooltip="Delete Account" icon={<LuBan size={18} />} onClick={() => setDeleteAccount(true)} />
+                            </div>
                         </div>
 
-                        <div className="flex flex-col items-center justify-center gap-3 my-3">
-                            {userProfile?.profileImageUrl ? (
-                                <img
-                                    src={userProfile?.profileImageUrl}
-                                    alt="Profile"
-                                    className="w-50 h-50 bg-state-400 rounded-full"
-                                />) : (
-                                <CharAvatar
-                                    fullName={userProfile?.fullName}
-                                    width="w-50"
-                                    height="h-50"
-                                    style="text-8xl"
-                                />
-                            )}
-                        </div>
+                        {userProfile?.profileImageUrl ? (
+                            <img
+                                src={userProfile?.profileImageUrl}
+                                alt="Profile"
+                                className="w-42 h-42 bg-state-400 rounded-full"
+                            />
+                        ) : (
+                            <CharAvatar
+                                fullName={userProfile?.fullName}
+                                width="w-42"
+                                height="h-42"
+                                style="text-6xl"
+                            />
+                        )}
 
                         <div className="w-full">
                             <Textarea
@@ -72,28 +116,8 @@ const Profile = () => {
                                 value={userProfile?.bio || "N/A"}
                                 placeholder="Write something about yourself..."
                                 allowInput={false}
-                                rows={6}
+                                rows={8}
                             />
-                        </div>
-
-                        <div className="flex flex-col items-center justify-center w-full gap-3">
-                            <div className="grid grid-cols-1 md:grid-cols-3 gap-3 w-full">
-                                <SettingButton
-                                    text="Update Profile"
-                                    onClick={handleUpdateProfile}
-                                    color="primary"
-                                />
-                                <SettingButton
-                                    text="Clear Transactions"
-                                    onClick={handleClearAllTransactions}
-                                    color="warning"
-                                />
-                                <SettingButton
-                                    text="Delete Account"
-                                    onClick={handleDeleteAccount}
-                                    color="error"
-                                />
-                            </div>
                         </div>
                     </div>
                     <div className="card">
@@ -118,6 +142,32 @@ const Profile = () => {
                     </div>
                 </div>
             </div>
+
+            <Modal
+                isOpen={clearAllTransactions}
+                onClose={() => setClearAllTransactions(false)}
+                title="Clear All Transactions Data"
+            >
+                <ConfirmAlert
+                    content="Are you sure you want to delete all transactions data?"
+                    onConfirm={() => { handleClearAllTransactions() }}
+                    confirmContent="Delete"
+                    color="error"
+                />
+            </Modal>
+
+            <Modal
+                isOpen={deleteAccount}
+                onClose={() => setDeleteAccount(false)}
+                title="Delete Account"
+            >
+                <ConfirmAlert
+                    content="Are you sure you want to delete your account?"
+                    onConfirm={() => { handleDeleteAccount() }}
+                    confirmContent="Delete"
+                    color="error"
+                />
+            </Modal>
         </DashboardLayout >
     );
 }
