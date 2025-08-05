2d2a8eed23619b7aa74ffcd103a3a9d47c8400ad
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-05 09:30:32 +0700

    [backend][routes] Update expenseRoutes with delete/all to delete all expenses

--- BackEnd/routes/expenseRoutes.js
+++ BackEnd/routes/expenseRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const {
   addExpense,
   getAllExpense,
-  deleteExpense,
+  deleteAllExpenses,
+  deleteExpenseWithID,
   downloadExpenseExcel,
 } = require("../controllers/expenseController");
 const { protect } = require("../middleware/authMiddleware");
@@ -12,6 +13,7 @@ const router = express.Router();
 router.post("/add", protect, addExpense);
 router.get("/get", protect, getAllExpense);
 router.get("/downloadexcel", protect, downloadExpenseExcel);
-router.delete("/:id", protect, deleteExpense);
+router.delete("/all", protect, deleteAllExpenses);
+router.delete("/:id", protect, deleteExpenseWithID);
 
 module.exports = router;
